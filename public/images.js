5ff25ff5fa3df5620de4820047cd541499dc20fe
Robert Renbris <robert.wensman@gmail.com>
2019-09-30 19:07:37 +0200

    ...

--- public/images.js
+++ public/images.js
@@ -40,3 +40,114 @@ let images = {
   darkSteelWall : loadImage("./images/steel_dark.png"),
   oldSteelWall : loadImage("./images/steel_wall_1_32x32.png")
 };
+
+
+function worldImageDimensions(imageElement) {
+  let width = imageElement.naturalWidth; 
+  let height = imageElement.naturalHeight;
+  const imageData = getImageData(imageElement);
+
+  // Get actual map height
+  let actualMapHeight;
+  let yScan = 0
+  while(yScan < height) {
+    if ("legendSeparator" === getColorCode(imageData, width, 0, yScan)) { 
+      actualMapHeight = yScan;
+      break;
+    };
+    yScan++;
+  }
+  actualMapHeight = yScan;
+  return {width: width, height: actualMapHeight};
+}
+
+function generateWorldFromImage(imageElement, tileSize, defaultColorCode, addItem) {
+  let width = imageElement.naturalWidth; 
+  let height = imageElement.naturalHeight;
+  
+  let hasReachedLegend = false;
+  
+  let visitedMap = {};  
+  function visited(x, y) {
+    return typeof(visitedMap[key(x, y)]) !== 'undefined';
+  }
+    
+  function key(x, y) {
+    return "(" + x + "," + y + ")";
+  }
+  
+  const imageData = getImageData(imageElement);
+  function startCollecting(x, y) {
+    if (!visited(x, y) && !hasReachedLegend) {
+      visitedMap[key(x, y)] = true;
+      let code = getColorCode(imageData, width, x, y);
+      if (defaultColorCode === null) defaultColorCode = code; // Just pick the first one. 
+      if (code === "legendSeparator") {
+        hasReachedLegend = true; 
+      } else if (code !== "space") {
+        let topLeftX = x;
+        let topLeftY = y;
+        let bottomRightX = x;
+        let bottomRightY = y;
+        
+        let finished = false;
+        while(!finished) {
+          let expand = false;
+          
+          // Try expand right
+          let scanX = bottomRightX + 1;
+          if (scanX < width) {
+            let allTheSame = true;
+            for (let scanY = topLeftY; scanY <= bottomRightY; scanY++) {
+              if (code !== getColorCode(imageData, width, scanX, scanY) || visited(scanX, scanY)) allTheSame = false;
+            }
+            if (allTheSame) {
+              // log("expand right");
+              expand = true;
+              bottomRightX++;
+              for (let scanY = topLeftY; scanY <= bottomRightY; scanY++) {
+                visitedMap[key(scanX, scanY)] = true;
+              }
+            }            
+          }
+
+          // Try expand right
+          scanY = bottomRightY + 1;
+          if (scanY < height) {           
+            allTheSame = true;
+            for (let scanX = topLeftX; scanX <= bottomRightX; scanX++) {
+              if (code !== getColorCode(imageData, width, scanX, scanY) || visited(scanX, scanY)) allTheSame = false;
+            }
+            if (allTheSame) {
+              // log("expand left");
+              expand = true;
+              bottomRightY++;
+              for (let scanX = topLeftX; scanX <= bottomRightX; scanX++) {
+                visitedMap[key(scanX, scanY)] = true;
+              }
+            }
+          }
+          finished = !expand;
+        }
+
+        addItem({
+          code: code,
+          defaultColorCode: defaultColorCode,
+          shapeX: topLeftX * tileSize,
+          shapeY: topLeftY * tileSize,
+          shapeWidth: (bottomRightX - topLeftX + 1) * tileSize,
+          shapeHeight: (bottomRightY - topLeftY + 1) * tileSize
+        });
+      }
+    }
+  }
+  
+  
+  // Loop over each pixel and invert the color.
+  for(let y = 0 ;y <= height; y++) {
+    for(let x = 0 ;x <= width; x++) {
+      //log("collecting..." + x + ", " + y + ":" + getColorCode(x, y));
+      startCollecting(x, y);
+    }
+  }
+}
